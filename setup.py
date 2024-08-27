6a50cba08df717dc406cbb84cf3408190105eb7c
周腾 <233151@nbcb.com>
2024-08-27 16:11:29 +0800

    🔖 chore(version): 更新版本至 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='auto_coder_chat_lite',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(where='.'),
     package_dir={'': '.'},
     include_package_data=True,
